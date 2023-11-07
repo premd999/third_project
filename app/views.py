7d17dee894dfd4981fd3a52ab5af59e1e5c16df9
premd999 <premdpk999@gmail.com>
2023-11-07 22:27:11 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def varma(request):
+    return HttpResponse('Worth Varma Worth.....MindBlowingg')
